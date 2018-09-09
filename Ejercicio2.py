e489bcf50d029fc6de26b0500de4c4c2726d0579
GuillermoCortes <42586963+GuillermoCortes@users.noreply.github.com>
2018-09-08 19:30:56 -0500

    Create Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: UTF-8-*-
+import turtle
+from turtle import *
+x=input('Escriba los lados de los poligonos pequeños: ')
+title("Ejercicio 2")
+penup()
+goto(-100,-200)
+pendown()
+turtle.backward
+def polpeq1(l):
+	g=1
+	while (g<=l):
+		turtle.forward(30)
+		turtle.left(360/l)
+		g=g+1
+def ubicar()
+  penup()
+  turtle.forward(100)
+  pendown()
+polpeq1(x)
+ubicar()
+polpeq1(x)
+turtle.left(90)
+ubicar()
+turtle.right(90)
+polpeq1()
+turtle.left(180)
+ubicar()
+turtle.left(180)
+polpeq1(x)
+raw_input("Pulse una tecla para continuar..") 
