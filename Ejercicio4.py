a425f1d9d10ee6006572984dddd9da2d43dd8b25
GuillermoCortes <42586963+GuillermoCortes@users.noreply.github.com>
2018-09-09 00:52:41 -0500

    Update Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -12,28 +12,29 @@ y=input('Escriba el numero de filas: ')
 def pol(l):
   h=1
   while(h<=l):
-    turtle.forward(20)
-    turtle.left(360/l)
-    h=h+1
+	turtle.forward(20)
+	turtle.left(360/l)
+	h=h+1
 def fila(l,f):
-  g=1
-  while(g<=f):
-    pol(l)
-    penup()
-    turtle.forward(30)
-    pendown()
-    g=g+1
- s=y
- while(s>=1):
-  fila(x,s)
-  turtle.left(180)
-  penup()
-  turtle.forward((s-1)*30)
-  turtle.left(180)
-  turtle.forward(15)
-  turtle.left(90)
-  turtle.forward(30)
-  turtle.right(90)
-  pendown()
-  s=s-1
- raw_input("Pulse una tecla para continuar..") 
+ g=1
+ while(g<f):
+   pol(l)
+   penup()
+   turtle.forward(30)
+   pendown()
+   g=g+1
+ pol(l)
+s=y
+while(s>=1):
+ fila(x,s)
+ turtle.left(180)
+ penup()
+ turtle.forward((s-1)*30)
+ turtle.left(180)
+ turtle.forward(15)
+ turtle.left(90)
+ turtle.forward(30)
+ turtle.right(90)
+ pendown()
+ s=s-1
+raw_input("Pulse una tecla para continuar..") 
