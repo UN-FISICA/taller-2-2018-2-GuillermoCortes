0bc5ae7612b1fb6a9af030061a13c3f1aff12c0e
GuillermoCortes <42586963+GuillermoCortes@users.noreply.github.com>
2018-09-09 01:15:24 -0500

    Update Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -3,12 +3,12 @@
 import turtle
 import math
 from turtle import *
-title("Ejercicio 5")
+title("Ejercicio 6")
 penup()
 goto(-100,-200)
 pendown()
 turtle.backward
-y=input('Escriba el numero de filas: ')
+y=input('Escriba el numero de filas de la pirámide: ')
 def pol(l,f):
   h=1
   while(h<=l):
@@ -16,24 +16,25 @@ def pol(l,f):
     turtle.left(360/l)
     h=h+1
 def fila(l,f):
-  g=1
-  while(g<=f):
-    pol(l,f)
-    penup()
-    turtle.forward((10*math.sqrt(2(1-math.cos((2*math.pi)/(f+2)))))+20)
-    pendown()
-    g=g+1
- s=y
- while(s>=1):
+	g=1
+	while(g<f):
+		pol(l)
+		penup()
+		turtle.forward((10*math.sqrt(2(1-math.cos((2*math.pi)/(f+2)))))+20)
+		pendown()
+		g=g+1
+	pol(l,f)
+s=y
+while(s>=1):
   fila(s+2,s)
   turtle.left(180)
   penup()
-  turtle.forward((s-1)*)
+  turtle.forward((s-1)*((10*math.sqrt(2(1-math.cos((2*math.pi)/(f+2)))))+20))
   turtle.left(180)
-  turtle.forward(15)
+  turtle.forward(((10*math.sqrt(2(1-math.cos((2*math.pi)/(f+2)))))+20)/2)
   turtle.left(90)
-  turtle.forward(30)
+  turtle.forward(40)
   turtle.right(90)
   pendown()
   s=s-1
- raw_input("Pulse una tecla para continuar..") 
+raw_input("Pulse una tecla para continuar..") 
